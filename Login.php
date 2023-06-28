8ac97f5725257ebfb02727f7271423d05371ee80
Murilo Pacheco <88802383+MuriloPacheco91@users.noreply.github.com>
2023-06-27 22:32:13 -0300

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+      <meta charset="utf-8">
+      <title>BookStock</title>
+      <link rel="stylesheet" href="estilo.css">
+      <link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+    </head>
+    <body>
+        <div Class="bookstockdiv"><p Class="bookstock">BookStock<p></div>
+        <div Class="login">
+          
+        <div class="card-content">
+
+            <div class="card-content-area">
+
+                <label for="usuario"><i></i>Usuário:</label>
+
+                <input type="text" id="usuario" autocomplete="on">
+
+            </div>
+
+            <div class="card-content-area2">
+
+                <label for="password">Senha:</label>
+
+                <input type="password" id="password" autocomplete="off">
+
+            </div>
+
+            </div>
+
+            <div class="card-footer">
+
+            <Buttom type="submit" class="Buttom">
+              <P Class="Logar">Logar</P>
+            </Buttom>
+
+            
+
+            </div>
+          
+          </div>
+        </div>
+        
+    </body>
+</html>
